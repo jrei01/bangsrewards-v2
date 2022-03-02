b413fe5471b1d1a6ed9e5e06a9e361ee23e76f27
unknown <preigarcia01@gmail.com>
2022-03-02 15:57:46 +0800

    added tailwind unpkg

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -19,6 +19,9 @@
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer> </script>
 
+        <!-- Tailwind -->
+        <link rel="stylesheet" href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css">
+
         @livewireStyles
 
     </head>
