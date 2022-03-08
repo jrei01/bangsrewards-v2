b92526f191c421f8df33e1f4e03a526ad05638f3
unknown <preigarcia01@gmail.com>
2022-03-08 12:42:29 +0800

    t

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -11,13 +11,13 @@
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"> 
 
         <!-- Styles -->
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ url('css/app.css') }}" rel="stylesheet">
 
         <!-- Alpine -->
         <script defer src="https://unpkg.com/alpinejs@3.8.1/dist/cdn.min.js"></script>
        
         <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer> </script>
+        <script src="{{ url('js/app.js') }}" defer> </script>
 
         <!-- Tailwind -->
         <link rel="stylesheet" href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css">
